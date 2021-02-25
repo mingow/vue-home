93a55bf8242eca5d33a727b3959f154a30e96297
wangmeng <>
2021-02-25 22:53:59 +0800

    remove unused files,add iconfont

--- src/App.vue
+++ src/App.vue
@@ -4,57 +4,48 @@
       app
       color="primary"
       dark
+      dense
+      clipped-left
+      elevation="0"
     >
       <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
+        <v-icon>icon-MES</v-icon>
       </div>
-
       <v-spacer></v-spacer>
-
       <v-btn
         href="https://github.com/vuetifyjs/vuetify/releases/latest"
         target="_blank"
         text
       >
-        <span class="mr-2">Latest Release</span>
+        <span class="mr-2">离开</span>
         <v-icon>mdi-open-in-new</v-icon>
       </v-btn>
     </v-app-bar>
-
+    <v-navigation-drawer permanent clipped>
+      
+    </v-navigation-drawer>
     <v-main>
-      <HelloWorld/>
+      
     </v-main>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
-
+import logo from './assets/logo.svg'
 export default {
   name: 'App',
 
   components: {
-    HelloWorld,
+    
   },
-
+  metaInfo:{
+    title:'制造执行系统',
+    htmlAttrs:{
+      lang:'cn'
+    }
+  },
   data: () => ({
-    //
+    logo:logo
   }),
 };
 </script>
